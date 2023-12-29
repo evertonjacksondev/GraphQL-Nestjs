6c564120fa5367e3684621ab6264475a4b7edac7
Everton Jackson <dev.evertonjackson@gmail.com>
2023-12-29 17:09:30 -0300

    feat: add module graphql nestjs

---
+++ src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+
+@Module({
+  imports: [GraphQLModule.forRoot<ApolloDriverConfig>({
+    driver: ApolloDriver,
+    playground: true,
+  }),],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
